767a422004a2c12f9970e0a216c8aaaca5222459
Ian Knox <iknox@ian-knox.attlocal.net>
2022-02-20 15:40:47 -0600

    script

---
+++ checkenv.sh
@@ -0,0 +1,37 @@
+# goals:
+# 1:  Ensure the right ssh command is in place env var!  
+# Use direnv!
+# 2: Ensure the right email is set in .git/config
+# Use `git config`
+
+# is envrc installed?
+# is asdf installed?
+# do we have a .envrc file?
+# do we have a .asdf file?
+
+
+# Help text
+if [[ $1 == "-h"  ]]; then
+    echo "help stuff goes here"
+fi
+
+# Run main 
+if [[ ! -n $1 ]]
+    
+    
+    # Do we have a global 
+
+
+
+
+# do we have a GIT_SSH_COMMAND
+#if [ ! -n "$GIT_SSH_COMMAND" ]; then
+#    echo "git ssh cmd not set, setting it"
+#    echo "fark" >> .envrc
+#    direnv allow .envrc
+#else
+#    echo "git ssh cmd is set"
+#fi
+
+
+
